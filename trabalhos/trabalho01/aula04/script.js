72b020e4eae23d2c9a95067b29191ee319a3b166
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-08-22 23:34:39 -0300

    trabalho contatos

---
+++ trabalhos/trabalho01/aula04/script.js
@@ -0,0 +1,61 @@
+function start() {
+    let bd_contacts = []
+    try {
+        getLocalStorage()
+        updateTable()
+    } catch {
+        setLocalStorage(bd_contacts)
+}}
+
+function inserirContato() {
+    const contact = {
+        name: document.getElementById("nome").value,
+        phone: document.getElementById("telefone").value
+    }
+
+    bd_contacts = getLocalStorage();
+    bd_contacts.push(contact);
+    setLocalStorage(bd_contacts);
+    updateTable();
+}
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem("bd_contacts"));
+}
+
+function setLocalStorage(bd_contacts) {
+    localStorage.setItem("bd_contacts", JSON.stringify(bd_contacts));
+}
+
+function updateTable() {
+    const bd_contacts = getLocalStorage();
+    cleanTable()
+    bd_contacts.forEach(newRow);
+}
+
+function newRow(contact, index) {
+    const line = document.createElement("tr");
+    line.innerHTML = `
+    <td>${index}</td>
+    <td>${contact.name}</td>
+    <td>${contact.phone}</td>
+    <td><button class="delete" onclick="deleteEntry(${index})"">Delete</button></td>
+    `;
+
+    document.querySelector("#contactsTable>tbody").appendChild(line);
+}
+
+function cleanTable() {
+    document.querySelector("#contactsTable>tbody").innerHTML = ""
+}
+
+function deleteEntry(index) {
+    bd_contacts = getLocalStorage();
+    bd_contacts[index] = ""
+    bd_contacts = bd_contacts.filter(function(entry) {
+        return entry !== ""
+    })
+    setLocalStorage(bd_contacts)
+    alert(`Entrada deletada com sucesso`)
+    updateTable()
+}
